76f20d031d143b1042f2999620edeef56a6fcaa1
Banki-r <benedek.banki@protonmail.com>
2025-10-17 16:51:40 +0200

    Add HelloWorld GUI and comment out memory-anaylzer, TODO: connect the two

--- control/Main.cpp
+++ control/Main.cpp
@@ -1,8 +1,10 @@
+#include <gtkmm/application.h>
 
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
 
 #include "FrontendFactoryWrapper.h"
+#include "view/MainWindow.h"
 
 #include <memory>
 
@@ -10,9 +12,13 @@ using namespace clang::tooling;
 using namespace llvm;
 
 static llvm::cl::OptionCategory toolCategory("class-analyzer <options>");
+int main(int argc, char **argv) {
+  auto app = Gtk::Application::create("memory anaylzer");
+  app->make_window_and_run<MainWindow>(argc, argv);
 
-int main(int argc, const char **argv) {
-  auto argsParser = CommonOptionsParser::create(argc, argv, toolCategory);
+
+  /*
+  auto argsParser = CommonOptionsParser::create(argc, const_cast<const char**>(argv), toolCategory);
   if (!argsParser) {
     llvm::errs() << argsParser.takeError();
     return -1;
@@ -21,6 +27,7 @@ int main(int argc, const char **argv) {
   ClangTool tool(optionsParser.getCompilations(),
                  optionsParser.getSourcePathList());
   auto myActionFactory = std::make_unique<FrontendFactoryWrapper>();
-
-  return tool.run(myActionFactory.get());
+  tool.run(myActionFactory.get());
+  */
+  return 0;
 }
